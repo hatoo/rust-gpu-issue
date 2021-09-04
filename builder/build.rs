8bf3951a3e1d4c8ea03bb383f7baea126553f3b6
hatoo <hato2000@gmail.com>
2021-09-04 21:43:33 +0900

    Update

--- builder/build.rs
+++ builder/build.rs
@@ -4,6 +4,8 @@ use spirv_builder::{Capability, MetadataPrintout, SpirvBuilder};
 
 fn main() -> Result<(), Box<dyn Error>> {
     SpirvBuilder::new("../shader", "spirv-unknown-vulkan1.2")
+        // OK
+        // SpirvBuilder::new("../shader", "spirv-unknown-spv1.3")
         .capability(Capability::RayTracingKHR)
         .extension("SPV_KHR_ray_tracing")
         .print_metadata(MetadataPrintout::Full)
